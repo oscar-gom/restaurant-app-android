31f745918f91a191c885e8d462cbebcffa54e1a9
Oscar Gomez <oscargomezsedas@gmail.com>
2025-11-10 12:44:12 +0100

    feat: add MenuItemRepository and OrderRepository for managing menu items and orders

---
+++ app/src/main/java/com/osg/restaurantcompanionapp/repository/MenuItemRepository.kt
@@ -0,0 +1,32 @@
+package com.osg.restaurantcompanionapp.repository
+
+import com.osg.restaurantcompanionapp.model.MenuItem
+import com.osg.restaurantcompanionapp.network.ApiService
+
+class MenuItemRepository(val apiService: ApiService) {
+
+    suspend fun getMenuItems(): List<MenuItem>? {
+        val response = apiService.getMenuItems()
+        return if (response.isSuccessful) response.body() else null
+    }
+
+    suspend fun getMenuItemById(id: Int): MenuItem? {
+        val response = apiService.getMenuItemById(id)
+        return if (response.isSuccessful) response.body() else null
+    }
+
+    suspend fun createMenuItem(menuItem: MenuItem): MenuItem? {
+        val response = apiService.createMenuItem(menuItem)
+        return if (response.isSuccessful) response.body() else null
+    }
+
+    suspend fun updateMenuItem(id: Int, menuItem: MenuItem): MenuItem? {
+        val response = apiService.updateMenuItem(id, menuItem)
+        return if (response.isSuccessful) response.body() else null
+    }
+
+    suspend fun deleteMenuItem(id: Int): Boolean {
+        val response = apiService.deleteMenuItem(id)
+        return response.isSuccessful
+    }
+}
---
+++ app/src/main/java/com/osg/restaurantcompanionapp/repository/OrderRepository.kt
@@ -0,0 +1,32 @@
+package com.osg.restaurantcompanionapp.repository
+
+import com.osg.restaurantcompanionapp.model.Order
+import com.osg.restaurantcompanionapp.network.ApiService
+
+class OrderRepository(private val apiService: ApiService) {
+    suspend fun getOrders(): List<Order>? {
+       val response = apiService.getOrders()
+        return if (response.isSuccessful) response.body() else null
+    }
+
+    suspend fun getOrderById(id: Int): Order? {
+        val response = apiService.getOrderById(id)
+        return if (response.isSuccessful) response.body() else null
+    }
+
+    suspend fun createOrder(order: Order): Order? {
+        val response = apiService.createOrder(order)
+        return if (response.isSuccessful) response.body() else null
+    }
+
+    suspend fun updateOrder(id: Int, order: Order): Order? {
+        val response = apiService.updateOrder(id, order)
+        return if (response.isSuccessful) response.body() else null
+    }
+
+    suspend fun deleteOrder(id: Int): Boolean {
+        val response = apiService.deleteOrder(id)
+        return response.isSuccessful
+    }
+
+}
