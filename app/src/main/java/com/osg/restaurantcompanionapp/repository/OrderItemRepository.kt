0b6ed42e854916baf20a19908df2081206ad75a9
Oscar Gomez <oscargomezsedas@gmail.com>
2025-11-10 12:54:57 +0100

    feat: add OrderItemRepository for managing order items

---
+++ app/src/main/java/com/osg/restaurantcompanionapp/repository/OrderItemRepository.kt
@@ -0,0 +1,41 @@
+package com.osg.restaurantcompanionapp.repository
+
+import com.osg.restaurantcompanionapp.model.OrderItem
+import com.osg.restaurantcompanionapp.network.ApiService
+
+class OrderItemRepository(val apiService: ApiService) {
+    suspend fun getOrderItems(): List<OrderItem>? {
+        val response = apiService.getOrderItems()
+        return if (response.isSuccessful) response.body() else null
+    }
+
+    suspend fun getOrderItemByIds(orderId: Int, menuItemId: Int): OrderItem? {
+        val response = apiService.getOrderItemByIds(orderId, menuItemId)
+        return if (response.isSuccessful) response.body() else null
+    }
+
+    suspend fun getOrderItemsByOrderId(orderId: Int): List<OrderItem>? {
+        val response = apiService.getOrderItemsByOrderId(orderId)
+        return if (response.isSuccessful) response.body() else null
+    }
+
+    suspend fun getOrderItemsByMenuItemId(menuItemId: Int): List<OrderItem>? {
+        val response = apiService.getOrderItemsByMenuItemId(menuItemId)
+        return if (response.isSuccessful) response.body() else null
+    }
+
+    suspend fun createOrderItem(orderItem: OrderItem): OrderItem? {
+        val response = apiService.createOrderItem(orderItem)
+        return if (response.isSuccessful) response.body() else null
+    }
+
+    suspend fun updateOrderItem(orderId: Int, menuItemId: Int, orderItem: OrderItem): OrderItem? {
+        val response = apiService.updateOrderItem(orderId, menuItemId, orderItem)
+        return if (response.isSuccessful) response.body() else null
+    }
+
+    suspend fun deleteOrderItem(orderId: Int, menuItemId: Int): Boolean {
+        val response = apiService.deleteOrderItem(orderId, menuItemId)
+        return response.isSuccessful
+    }
+}
