31766941a3fa65f18227524a8d344743051a23c4
Oscar Gomez <oscargomezsedas@gmail.com>
2025-11-14 13:18:40 +0100

    feat: add OrdersView and integrate it into MainActivity

--- app/src/main/java/com/osg/restaurantcompanionapp/MainActivity.kt
+++ app/src/main/java/com/osg/restaurantcompanionapp/MainActivity.kt
@@ -4,19 +4,14 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.osg.restaurantcompanionapp.navigation.NavItem
 import com.osg.restaurantcompanionapp.ui.theme.RestaurantCompanionAppTheme
+import com.osg.restaurantcompanionapp.view.OrdersView
 import com.osg.restaurantcompanionapp.view.component.BaseScaffold
 import com.osg.restaurantcompanionapp.viewmodel.OrderViewModel
 
@@ -43,7 +38,7 @@ fun AppRoot() {
                 current = NavItem.Orders,
                 onAdd = { vm.onAdd() }
             ) {
-                OrderListView(
+                OrdersView(
                     modifier = it,
                     viewModel = vm
                 )
---
+++ app/src/main/java/com/osg/restaurantcompanionapp/view/OrdersView.kt
@@ -0,0 +1,10 @@
+package com.osg.restaurantcompanionapp.view
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavBackStackEntry
+import com.osg.restaurantcompanionapp.viewmodel.OrderViewModel
+
+@Composable
+fun OrdersView(modifier: NavBackStackEntry, viewModel: OrderViewModel) {
+
+}
